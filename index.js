7db609321ea81b914c5619bd8eae9ce10a23371e
Kath_code <kathmene27@gmail.com>
2020-05-24 21:51:13 -0500

    [Step 3] - Create my public functions to update my private settings object.

--- index.js
+++ index.js
@@ -11,6 +11,38 @@
       take: 100,
     };
 
+    // Change a private property of the library
+    _myLibraryObject.setPageSize = function (pageSize) {
+      if (typeof pageSize === 'number') {
+        settings.pageSize = pageSize;
+      } else {
+        console.log('You should send a number to change the page size');
+      }
+
+      return pageSize;
+    };
+
+    // Change a private property of the library
+    _myLibraryObject.setSkip = function (skip) {
+      if (typeof skip === 'number') {
+        settings.skip = skip;
+      } else {
+        console.log('You should send a number to change the skip');
+      }
+
+      return skip;
+    };
+
+    _myLibraryObject.setTake = function (take) {
+      if (typeof take === 'number') {
+        settings.take = take;
+      } else {
+        console.log('You should send a number to change the take');
+      }
+
+      return take;
+    };
+
     return _myLibraryObject;
   }
 
@@ -21,3 +53,4 @@
 })(window);
 
 console.log(myWindowGlobalLibraryName);
+// PUBLIC => {setPageSize: ƒ, setSkip: ƒ, setTake: ƒ, getPageSize: ƒ}
